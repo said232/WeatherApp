38b91f6ae9f33136bc5e9e8870b5b5e95f304e36
said232 <saidbilalocc@gmail.com>
2020-01-14 20:42:54 +0100

    newchangesonjs

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// api key : 82005d27a116c2880c8f0fcb866998a0
+ let latitude;
+ let longitude;
+ const notification = document.getElementsByClassName("notification")[0];
+function getLocation() {
+    console.log(navigator.geolocation);
+    navigator.geolocation.getCurrentPosition(onSccess,onError);
+    
+}
+
+function onSccess(position)
+{
+    console.log(position);
+    
+}
+function onError(error)
+{
+const p =document.createElement('p');
+    console.log('no what did you do is ',error);
+    p.innerHTML=error.message
+    notification.style.display='block'
+    notification.appendChild(p)
+}
+getLocation()
