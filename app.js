e4173a0a59b444341ae73195262b65c50af84d0d
said232 <saidbilalocc@gmail.com>
2020-01-18 10:42:19 +0100

    finshed changes

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+const apikey = 'b9432112fd2bfe2facd7552071a813a1';
  let latitude;
  let longitude;
- const notification = document.getElementsByClassName("notification")[0];
+let temperElemnt = document.querySelector('.temperature-value p');
+const notification = document.getElementsByClassName('notification')[0];
+let weatherIcon = document.querySelector('.weather-icon');
+let locationElement = document.querySelector('.location p');
+let temperatureDescription = document.querySelector('.temperature-description p');
+
+
 function getLocation() {
     console.log(navigator.geolocation);
     navigator.geolocation.getCurrentPosition(onSccess,onError);
@@ -10,8 +17,26 @@ function getLocation() {
 function onSccess(position)
 {
     console.log(position);
+    latitude =position.coords.latitude
+    longitude = position.coords.longitude
+    const weather = fetch('https://api.openweathermap.org/data/2.5/weather?'
+    +'lat='
+    +latitude
+    +'&lon='
+    +longitude
+    +'&appid='
+    +apikey
     
+    );
+    weather
+        .then(response => response.json())
+        .then(result => 
+            {
+                temperElemnt.innerHTML = `${Math.round(result.main.temp - 273.15)} °<span>C</span>`;
+                weatherIcon.innerHTML = `<img src="icons/${result.weather[0].icon}.png"/>`; locationElement.innerHTML = `${result.name + ' ' + result.sys.country}`;temperatureDescription.innerHTML = `${result.weather[0].description}`;
+            });
 }
+
 function onError(error)
 {
 const p =document.createElement('p');
